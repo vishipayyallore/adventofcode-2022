b005fa1855b5b2674771533108245a9dd5ee5f27
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-01 21:22:21 +0530

    Day 1 - Part 2

--- day1/main.go
+++ day1/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Println("Advent of Code - Day 1 - Go - 2022")
 
-	fileNameWithPath := "./data/input.txt" // "./data/input.txt" OR "./data/sample.txt"
+	fileNameWithPath := "./data/sample.txt" // "./data/input.txt" OR "./data/sample.txt"
 
 	lines := utilities.GetInput(fileNameWithPath)
 	// fmt.Println(lines)  // Print all lines. Only for debug
@@ -16,4 +16,12 @@ func main() {
 	calories := utilities.SolveProblem(lines)
 
 	fmt.Println("Calories: ", calories)
+
+	topThreeCalories := utilities.SolveTopThreeProblem(lines)
+
+	fmt.Println("Top three Calories: ", topThreeCalories)
 }
+
+// topThreeCalories := make([]int, 3)
+
+// fmt.Printf("%v %v %v", topThreeCalories[0], topThreeCalories[1], topThreeCalories[2])
